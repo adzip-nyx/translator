062b6c95a619790249f77e76492e5c46a9f93243
Mr.Arm <52254990+Tim408@users.noreply.github.com>
2023-12-12 20:19:29 +0300

    Правка бага

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 from dotenv import load_dotenv
-import os, json, eel
+import os, eel
 
 load_dotenv()
 client = OpenAI(
@@ -9,8 +9,10 @@ client = OpenAI(
 
 print(1)
 eel.init('web')
-global tell, text
-text = ""
+
+global tell 
+
+
 @eel.expose
 def send_data(data):
     print(2)
@@ -19,14 +21,11 @@ def send_data(data):
         messages = [{"role": 'user', 'content': "tranlation "+ data[0] + "-" + data[1] + ": " + data[2]}],
         stream=False
     )
-    text = get_data(tell.choices[0].message.content)
-
+    print(tell.choices[0].message.content)
 
 @eel.expose
 def get_data():
-    print(text)
-    return text
-
-get_data()
+    print()
+    return tell.choices[0].message.content
 
 eel.start('index.html')
