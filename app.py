f4d567e54780601a1c3aa876799448d50c785b8a
Mr.Arm <52254990+Tim408@users.noreply.github.com>
2023-12-12 21:05:17 +0300

    Комментарии к работе

--- app.py
+++ app.py
@@ -1,25 +1,35 @@
 from openai import OpenAI
 from dotenv import load_dotenv
-import os, eel
-
+import os, eel 
+#библиотеки
 load_dotenv()
+#объявление api ключа для связи с ChatGPT
 client = OpenAI(
     api_key = os.getenv("API_KEY")
 )
 
 eel.init('web') 
 
+# get запрос по eel для передачи данных из js в python
 @eel.expose
 def get_data(data):
-        print(2)
+        #запрос у ChatGPT
         tell = client.chat.completions.create(
         model= "gpt-3.5-turbo",
         messages = [{"role": 'user', 'content': "tranlation "+ data[0] + "-" + data[1] + ": " + data[2]}],
         stream=False
         )
+        
+        #Парсер ответа
         text = tell.choices[0].message.content
-        if "Translation:" in text:
-            text = text[12::]
+        
+        # Замена возможных текстов из ChatGPT
+        if 'Translation: "' in text:
+            text = text[14:-1]
+        elif "Translation:" in text:
+            text = text[13:]
+        #Возврат данных обратно в js код
         return text
-
+    
+    
 eel.start('index.html')
