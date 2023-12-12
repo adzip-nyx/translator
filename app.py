6157a6ca093ce83da8bd7397e899fb8d8a5a9314
Mr.Arm <52254990+Tim408@users.noreply.github.com>
2023-12-12 16:39:00 +0300

    Тест связки через Json

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 from openai import OpenAI
 from dotenv import load_dotenv
-import os
+import os, sys, json
+
+
+input_data = sys.stdin.read()
+print(input_data)
+
+
+
 load_dotenv()
 client = OpenAI(
     api_key = os.getenv("API_KEY")
 )
+def translation(tol, inl, text):
+    tell = client.chat.completions.create(
+        model= "gpt-3.5-turbo",
+        messages = [{"role": 'user', 'content': "tranlation "+ tol + "-" + inl + ": " + text}],
+        stream=False
+        
+    )
+    return tell.choices[0].message.content
 
-text = "I am bad"
-tell = client.chat.completions.create(
-    model= "gpt-3.5-turbo",
-    messages = [{"role": 'user', 'content': "tranlation "+ "en" + "-" + "ru"+ ": " + text}],
-    stream=False
-    
-)
-print(tell.choices[0].message.content)
+
+out_text = translation("en", "ru", "text")
+
+output_json = json.dumps(out_text)
+print(output_json)
